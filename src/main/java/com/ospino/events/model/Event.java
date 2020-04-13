9e7836eaeeafa7970688ce3ddd037efc76bc7f4d
Ospino <ospino.p@gmail.com>
2020-04-13 20:11:57 +0200

    add model decoratos and id field
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/main/java/com/ospino/events/model/Event.java
+++ src/main/java/com/ospino/events/model/Event.java
@@ -1,63 +1,13 @@
 package com.ospino.events.model;
 
-
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue; // Auto-generated primary key
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Column;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
-import javax.persistence.Transient; // not saved in the DB
 
 @Entity
 public class Event {
 
     @Id
     @GeneratedValue
-    private long id; // ID and it is Generated (Increasing)
-
-    private String firstname;
-    private String lastname;
-    private String email;
-    private String telephone;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
+    private long id;
 }
--- src/main/java/com/ospino/events/model/User.java
+++ src/main/java/com/ospino/events/model/User.java
@@ -1,4 +1,13 @@
 package com.ospino.events.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
 }
