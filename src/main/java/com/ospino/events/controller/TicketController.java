3bb4e0883efa7ba80cd0600a910129e23040c262
Ospino <ospino.p@gmail.com>
2020-04-13 20:19:09 +0200

    add rest controller main decorators
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- src/main/java/com/ospino/events/controller/TicketController.java
+++ src/main/java/com/ospino/events/controller/TicketController.java
@@ -1,4 +1,10 @@
 package com.ospino.events.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tickets")
 public class TicketController {
+
 }
