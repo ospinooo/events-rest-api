4d8cd8488dc9102ad83968526481ce7e6a1daacc
Ospino <ospino.p@gmail.com>
2020-05-13 01:45:24 +0200

    add user model data and repository functions

--- src/main/java/com/ospino/events/model/User.java
+++ src/main/java/com/ospino/events/model/User.java
@@ -1,6 +1,9 @@
 package com.ospino.events.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -8,10 +11,23 @@ import java.util.Set;
 @Table(name="users")
 public class User {
 
+    /* GENERAL DATA */
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(min=6, max = 100)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
+
+    /* SPECIFIC DATA */
     @OneToMany(mappedBy = "user")
     private List<Ticket> tickets;
 
@@ -20,4 +36,44 @@ public class User {
             joinColumns = { @JoinColumn(name = "fk_user") },
             inverseJoinColumns = { @JoinColumn(name = "fk_event") })
     private Set<Event> events;
+
+    public User() {
+    }
+
+    public User(@NotBlank @Size(min = 3, max = 50) String username, @NotBlank @Size(min = 6, max = 100) String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
