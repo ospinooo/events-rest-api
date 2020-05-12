647c41ce5fd97246ed69558f833da34a0470be10
Ospino <ospino.p@gmail.com>
2020-05-13 01:45:01 +0200

    implement event routes

--- src/main/java/com/ospino/events/controller/EventController.java
+++ src/main/java/com/ospino/events/controller/EventController.java
@@ -3,6 +3,7 @@ package com.ospino.events.controller;
 import com.ospino.events.model.Event;
 import com.ospino.events.repository.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,19 +16,73 @@ public class EventController {
     @Autowired // Dependency injection
     private EventRepository eventRepository;
 
+    /**
+     * Get all events
+     * - Search Keys
+     * - Pagination
+     * @return
+     */
     @GetMapping
-    public List<Event> getAllEvents(){ return null; };
+    public List<Event> getAllEvents(){
+        return eventRepository.findAll();
+    };
 
+    /**
+     * All Event details
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
-    public Event getEvent(@PathVariable("id") long id){ return null; };
+    public Object getEvent(@PathVariable("id") long id){
+        return eventRepository.findById(id);
+    };
 
+
+    /**
+     * Create a new Event
+     * @param event
+     * @return
+     */
     @PostMapping
-    public ResponseEntity<Event> addEvent(){ return null; };
+    public ResponseEntity<Event> addEvent(@RequestBody Event event){
+        eventRepository.save(event);
+        return new ResponseEntity<Event>(HttpStatus.CREATED);
 
+    };
+
+    /**
+     * Update an event
+     * @param id
+     * @return
+     */
     @PutMapping("/{id}")
-    public ResponseEntity<Event> updateEvent(@PathVariable("id") long id){ return null; };
+    public ResponseEntity<Event> updateEvent(@PathVariable("id") long id){
+        Event event = eventRepository.findById(id);
+        if (event == null) {
+            System.out.println("Contact not found!");
+            return new ResponseEntity<Event>(HttpStatus.NOT_FOUND);
+        }
+        event.setId(id);
+        eventRepository.save(event);
+        return new ResponseEntity<Event>(HttpStatus.OK);
+    };
 
+
+    /**
+     * Delete an event
+     * @param id
+     * @return
+     */
     @DeleteMapping("/{id}")
-    public ResponseEntity<Event> deleteEvent(@PathVariable("id") long id){ return null; };
+    public ResponseEntity<Event> deleteEvent(@PathVariable("id") long id){
+        Event event = eventRepository.findById(id);
+        if (event == null) {
+            System.out.println("Contact not found!");
+            return new ResponseEntity<Event>(HttpStatus.NOT_FOUND);
+        }
+
+        eventRepository.deleteById(id);
+        return new ResponseEntity<Event>(HttpStatus.OK);
+    };
 
 }
--- src/main/java/com/ospino/events/model/Event.java
+++ src/main/java/com/ospino/events/model/Event.java
@@ -1,6 +1,7 @@
 package com.ospino.events.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.sql.Time;
 import java.util.List;
 import java.util.Set;
@@ -18,7 +19,39 @@ public class Event {
     private String description; // Markdown.
     private String color;
 
-    // Times
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    /* Times
     private Time init_time;
     private Time end_time;
     private TimeZone timezone;
@@ -27,6 +60,7 @@ public class Event {
     // Location
     private Long latitude;
     private Long longitude;
+
 
     @OneToMany(mappedBy = "event")
     private List<Fee> fees;
@@ -46,6 +80,7 @@ public class Event {
     // Extra
     // private Notification notification;
     // private Attachment attachment;
+    */
 }
 
 
