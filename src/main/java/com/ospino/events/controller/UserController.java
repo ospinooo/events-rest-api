195b2de2d619ce0e1e3a7c1a190531b811a45a53
Ospino <ospino.p@gmail.com>
2020-05-13 15:54:16 +0200

    add basic user routes

--- src/main/java/com/ospino/events/controller/UserController.java
+++ src/main/java/com/ospino/events/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.ospino.events.controller;
 
+import com.ospino.events.model.Event;
 import com.ospino.events.model.User;
 import com.ospino.events.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,19 +18,31 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public List<User> getAllUsers(){ return null; };
+    public List<User> getAllUsers(){ return userRepository.findAll(); };
 
     @GetMapping("/{id}")
-    public User getUser(@PathVariable("id") long id){ return null; };
+    public User getUser(@PathVariable("id") long id){ return userRepository.findById(id); };
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") long id){
+        User user = userRepository.findById(id);
+        if (user == null) {
+            System.out.println("User not found!");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+        userRepository.deleteById(id);
+        return new ResponseEntity<User>(HttpStatus.OK);
+    };
+
+
+    /*
     @PostMapping
     public ResponseEntity<User> addUser(){ return null; };
 
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable("id") long id){ return null; };
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<User> deleteUser(@PathVariable("id") long id){ return null; };
+    */
 
 
 }
